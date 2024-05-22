c3bd9227df6fe61d6cb8f7ad933b2c4ae3c73c63
hsiF <90235641+zrodevkaan@users.noreply.github.com>
2024-05-22 16:08:21 -0500

    Update devConsts.ts

--- src/distrust/devConsts.ts
+++ src/distrust/devConsts.ts
@@ -1,3 +1,5 @@
 ﻿import {CoreLogger} from "./api/logger";
+import {DataHandler} from "./renderer/managers/storage";
 
 export const coreLogger = new CoreLogger('distrust')
+export const generalSettings = new DataHandler('distrust')
