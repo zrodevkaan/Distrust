3d3ac2c5a6e435eab82e9a816723307013d954e0
hsiF <90235641+zrodevkaan@users.noreply.github.com>
2024-05-25 23:04:01 -0500

    dsfgf

--- src/distrust/renderer/mods/settings/components/CustomCSS.tsx
+++ src/distrust/renderer/mods/settings/components/CustomCSS.tsx
@@ -1,40 +1,90 @@
 ﻿import {modules} from '../../../../api/webpack/common';
 import {injectCSS, uninjectCSS} from "../../../../api/css";
 import {generalSettings} from "../../../../devConsts";
+import {getKeys} from "../../../../api/webpack/getters";
+
+const Modals = getKeys('Anchor');
+const {FormSwitch} = getKeys('FormSwitch');
 
 const CustomCSSEditor = () => {
     const [editorContent, setEditorContent] = modules.react.useState("/* Put your css here ;3 Not putting any css will do harm to my heart :( */");
+    const [liveUpdate, setLiveUpdate] = modules.react.useState(false);
 
     const script = document.createElement('script');
     script.src = "https://cdnjs.cloudflare.com/ajax/libs/ace/1.5.3/ace.js";
-    script.id = "aceEditor"
-    
+    script.id = "aceEditor";
+
     modules.react.useEffect(() => {
-        document.getElementById("aceEditor")?.remove?.();
-        // god this is so lazy.
-        script.onload = async () => {
-            const editor = window.ace.edit("editor");
-            editor.setTheme("ace/theme/cobalt");
-            editor.session.setMode("ace/mode/css");
-            editor.session.setValue(editorContent ?? await generalSettings.get('customCss'))
-            setEditorContent(editorContent ?? await generalSettings.get('customCss'))
-            editor.session.on("change", function () {
-                const newContent = editor.getValue();
-                setEditorContent(newContent);
-                void generalSettings.set('customCss', newContent)
-            });
+        const loadEditor = async () => {
+            document.getElementById("aceEditor")?.remove?.();
+            // god this is so lazy.
+            script.onload = async () => {
+                const editor = window.ace.edit("editor");
+                editor.setOptions({
+                    // maxLines: Infinity,  // this is going to be very slow on large documents
+                    useWrapMode: true,
+                    indentedSoftWrap: true,
+                    behavioursEnabled: true,
+                    enableBasicAutocompletion: true,
+                    enableSnippets: true,
+                    enableLiveAutocompletion: false
+                    // showLineNumbers: false, // hide the gutter
+                    // theme: "ace/theme/xcode"
+                });
+                editor.setTheme("ace/theme/cobalt");
+                editor.session.setMode("ace/mode/css");
+                const savedCss = await generalSettings.get('customCss');
+                editor.session.setValue(savedCss ?? editorContent);
+                setEditorContent(savedCss ?? editorContent);
+                editor.session.on("change", function () {
+                    const newContent = editor.getValue();
+                    setEditorContent(newContent);
+                    void generalSettings.set('customCss', newContent);
+                });
+            };
+            document.body.appendChild(script);
+
+            const liveUpdateSetting = await generalSettings.get('liveUpdateCss');
+            setLiveUpdate(liveUpdateSetting);
         };
-        document.body.appendChild(script);
+
+        void loadEditor();
     }, []);
 
     modules.react.useEffect(() => {
-        injectCSS('customCss', editorContent);
-    }, [editorContent]);
+        const updateLiveCSS = async () => {
+            if (liveUpdate) {
+                injectCSS('customCss', editorContent);
+            }
+        };
+
+        void updateLiveCSS();
+    }, [editorContent, liveUpdate]);
+
+    const handleToggleLiveUpdate = async () => {
+        const newLiveUpdate = !liveUpdate;
+        setLiveUpdate(newLiveUpdate);
+        await generalSettings.set('liveUpdateCss', newLiveUpdate);
+    };
 
     return (
-        <div className="editor-container">
-            <div id="editor">/* Put your css here ;3 Not putting any css will do harm to my heart :( */</div>
-        </div>
+        <>
+            <div className="editor-container">
+                <div id="editor">
+                    /* Put your css here ;3 Not putting any css will do harm to my heart :( */
+                </div>
+            </div>
+            <FormSwitch
+                note="Toggle live update for custom CSS"
+                value={liveUpdate}
+                onChange={handleToggleLiveUpdate}
+            >
+                Live Update CSS
+            </FormSwitch>
+            <Modals.Button onClick={() => injectCSS('customCss', editorContent)}>
+                Update CSS
+            </Modals.Button>
+        </>
     );
 };
 
